7969c4e4fbdc99aecdaa4c0ad5b2df10e9276248
robertmitchellv <robert.beaty.mitchell@gmail.com>
2025-02-12 10:46:26 -0800

    change span to h2

--- containers/ecr-viewer/src/app/page.tsx
+++ containers/ecr-viewer/src/app/page.tsx
@@ -48,7 +48,7 @@ const HomePage = async ({
       <Header />
       <main className="overflow-auto height-full">
         <div className="margin-x-3 padding-y-105 display-flex flex-align-center">
-          <span className="text-bold font-sans-xl">eCR Library</span>{" "}
+          <h2 className="text-bold font-sans-xl">eCR Library</h2>{" "}
           <LibrarySearch
             className="margin-left-auto"
             textBoxClassName="width-21-9375"
