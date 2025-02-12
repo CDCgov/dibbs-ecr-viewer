4147c41cfd66e74a4ca843a63e7e1811600a4303
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-02-12 14:14:12 -0500

    fix: use effect instead of force client on back button

--- containers/ecr-viewer/src/app/view-data/components/BackButton.tsx
+++ containers/ecr-viewer/src/app/view-data/components/BackButton.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { ArrowBack } from "@/app/components/Icon";
 import Link from "next/link";
 import classNames from "classnames";
 import { env } from "next-runtime-env";
 import { retrieveFromSessionStorage } from "../../components/utils";
-import { ForceClient } from "./ForceClient";
 
 interface BackButtonProps {
   className?: string;
   iconClassName?: string;
 }
 
-// Main implementation, rendered on the client only
-const ClientBackButton = ({ className, iconClassName }: BackButtonProps) => {
+/**
+ * Back button component for returning users from the eCR Viewer page to the eCR Library home page
+ * @param props - Properties for the back button
+ * @param props.className - Class names to be applied to the link
+ * @param props.iconClassName - Class names to be applied to the icon
+ * @returns <BackButton/> a react component back button
+ */
+export const BackButton = ({ className, iconClassName }: BackButtonProps) => {
+  const [savedUrlParams, setSavedUrlParams] = useState<string | null>(null);
   const isNonIntegratedViewer =
     env("NEXT_PUBLIC_NON_INTEGRATED_VIEWER") === "true";
 
-  const savedUrlParams = retrieveFromSessionStorage("urlParams");
+  useEffect(() => {
+    setSavedUrlParams(retrieveFromSessionStorage("urlParams") as string | null);
+  }, []);
 
   return (
     isNonIntegratedViewer && (
@@ -32,34 +41,5 @@ const ClientBackButton = ({ className, iconClassName }: BackButtonProps) => {
         Back to eCR Library
       </Link>
     )
-  );
-};
-
-// Fallback state for server and while loading the client code
-const LoadingBackButton = ({ iconClassName, className }: BackButtonProps) => {
-  return (
-    <div className={classNames("display-inline-block", className)}>
-      <ArrowBack
-        aria-label={"Back Arrow"}
-        size={3}
-        className={classNames("text-middle margin-right-1", iconClassName)}
-      />
-      Back to eCR Library
-    </div>
-  );
-};
-
-/**
- * Back button component for returning users from the eCR Viewer page to the eCR Library home page
- * @param props - Properties for the back button
- * @param props.className - Class names to be applied to the link
- * @param props.iconClassName - Class names to be applied to the icon
- * @returns <BackButton/> a react component back button
- */
-export const BackButton = (props: BackButtonProps) => {
-  return (
-    <ForceClient loading={<LoadingBackButton {...props} />}>
-      <ClientBackButton {...props} />
-    </ForceClient>
   );
 };
