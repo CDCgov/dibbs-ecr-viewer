ac182532e9dbb3006243a83507cdcf0e5f5141d5
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-02-18 16:43:34 -0500

    fix: only show NBS help text when integrated

--- containers/ecr-viewer/src/app/not-found.tsx
+++ containers/ecr-viewer/src/app/not-found.tsx
@@ -1,28 +1,36 @@
 import ErrorPage from "@/app/components/ErrorPage";
+import { env } from "next-runtime-env";
 
 /**
  * 404 page
  * @returns 404 Page
  */
-const NotFound = () => (
-  <ErrorPage
-    title="Page not found"
-    subTitle="The requested page could not be found"
-  >
-    Please try the following:
-    <ul className="margin-0 padding-left-3">
-      <li>
-        <b>Check the URL:</b> Make sure there are no typos in the address.
-      </li>
-      <li>
-        <b>Return to NBS:</b> Return to NBS and try to reopen the eCR.
-      </li>
-      <li>
-        <b>Contact support:</b> If the problem persists, please reach out to
-        your eCR coordinator.
-      </li>
-    </ul>
-  </ErrorPage>
-);
+const NotFound = () => {
+  const isNonIntegratedViewer =
+    env("NEXT_PUBLIC_NON_INTEGRATED_VIEWER") === "true";
+
+  return (
+    <ErrorPage
+      title="Page not found"
+      subTitle="The requested page could not be found"
+    >
+      Please try the following:
+      <ul className="margin-0 padding-left-3">
+        <li>
+          <b>Check the URL:</b> Make sure there are no typos in the address.
+        </li>
+        {!isNonIntegratedViewer && (
+          <li>
+            <b>Return to NBS:</b> Return to NBS and try to reopen the eCR.
+          </li>
+        )}
+        <li>
+          <b>Contact support:</b> If the problem persists, please reach out to
+          your eCR coordinator.
+        </li>
+      </ul>
+    </ErrorPage>
+  );
+};
 
 export default NotFound;
