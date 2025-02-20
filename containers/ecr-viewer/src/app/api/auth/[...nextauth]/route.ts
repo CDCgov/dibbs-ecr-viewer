26ecfe9074cd551427d99070cb68527b5478d134
Boban Ljuljdjurovic <boban@skylight.digital>
2025-02-20 08:28:09 -0500

    feat: add azure ad provider

--- containers/ecr-viewer/src/app/api/auth/[...nextauth]/route.ts
+++ containers/ecr-viewer/src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,29 @@
 import NextAuth from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
+import AzureAdProvider from "next-auth/providers/azure-ad";
 
-const handler = NextAuth({
-  providers: [
-    KeycloakProvider({
+const keycloak = () => {
+  if (
+    process.env.AUTH_KEYCLOAK_ID &&
+    process.env.AUTH_KEYCLOAK_SECRET &&
+    process.env.AUTH_KEYCLOAK_ISSUER
+  )
+    return KeycloakProvider({
       clientId: process.env.AUTH_KEYCLOAK_ID,
       clientSecret: process.env.AUTH_KEYCLOAK_SECRET,
       issuer: process.env.AUTH_KEYCLOAK_ISSUER,
-    }),
-  ],
+    });
+};
+const azure = () => {
+  if (process.env.AUTH_AZURE_AD_ID && process.env.AUTH_AZURE_AD_SECRET)
+    return AzureAdProvider({
+      clientId: process.env.AUTH_AZURE_AD_ID,
+      clientSecret: process.env.AUTH_AZURE_AD_SECRET,
+      tenantId: process.env.AUTH_AZURE_AD_TENANT_ID,
+    });
+};
+const handler = NextAuth({
+  providers: [keycloak(), azure()].filter((p) => p !== undefined),
   callbacks: {
     async redirect({ url, baseUrl }) {
       const defaultUrl = `${baseUrl}/ecr-viewer`;
