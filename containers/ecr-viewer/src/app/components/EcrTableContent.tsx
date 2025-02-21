36e33776cb0d9034fe02ac3c8b2024e120a21d5f
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-02-21 08:36:25 -0500

    refactor: simplify `SortButton` logic (#337)

--- containers/ecr-viewer/src/app/components/EcrTableContent.tsx
+++ containers/ecr-viewer/src/app/components/EcrTableContent.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { listEcrData } from "@/app/services/listEcrDataService";
-import { EcrTableClient } from "@/app/components/EcrTableClient";
-import { EcrTableNoData } from "@/app/components/EcrTableClientBase";
 import { DateRangePeriod } from "@/app/utils/date-utils";
+import { EcrTableDataRow } from "./EcrTableDataRow";
 
 /**
  * eCR Table
@@ -16,7 +15,7 @@ import { DateRangePeriod } from "@/app/utils/date-utils";
  * @param props.filterConditions - (Optional) The reportable condition(s) used to filter the data
  * @returns - eCR Table element
  */
-const EcrTable = async ({
+const EcrTableContent = async ({
   currentPage,
   itemsPerPage,
   sortColumn,
@@ -45,17 +44,13 @@ const EcrTable = async ({
     filterConditions,
   );
 
-  if (data.length === 0) {
-    return <EcrTableNoData />;
-  }
-
   return (
-    <EcrTableClient
-      data={data}
-      sortColumn={sortColumn}
-      sortDirection={sortDirection}
-    />
+    <tbody>
+      {data.map((item, index) => (
+        <EcrTableDataRow key={index} item={item} />
+      ))}
+    </tbody>
   );
 };
 
-export default EcrTable;
+export default EcrTableContent;
---
+++ containers/ecr-viewer/src/app/components/EcrTableDataRow.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { toSentenceCase } from "../utils/format-utils";
+import { saveToSessionStorage } from "../utils/storage-utils";
+import { noData } from "../utils/data-utils";
+import { useQueryParam } from "../hooks/useQueryParam";
+import { EcrDisplay } from "../services/listEcrDataService";
+
+/**
+ * Formats a single row of the eCR table.
+ * @param props - The properties passed to the component.
+ * @param props.item - The eCR data to be formatted.
+ * @returns A JSX table row element representing the eCR data.
+ */
+export const EcrTableDataRow = ({ item }: { item: EcrDisplay }) => {
+  const patient_first_name = toSentenceCase(item.patient_first_name);
+  const patient_last_name = toSentenceCase(item.patient_last_name);
+
+  const { searchParams } = useQueryParam();
+
+  const conditionsList = (
+    <ul className="ecr-table-list">
+      {item.reportable_conditions.map((rc, index) => (
+        <li key={index}>{rc}</li>
+      ))}
+    </ul>
+  );
+
+  const summariesList = (
+    <ul className="ecr-table-list">
+      {item.rule_summaries.map((rs, index) => (
+        <li key={index}>{rs}</li>
+      ))}
+    </ul>
+  );
+  const saveUrl = () => {
+    saveToSessionStorage("urlParams", searchParams.toString());
+  };
+
+  return (
+    <tr>
+      <td>
+        <Link onClick={saveUrl} href={`/view-data?id=${item.ecrId}`}>
+          {patient_first_name} {patient_last_name}
+        </Link>
+        {item.eicr_version_number && (
+          <span className="usa-tag margin-x-1 padding-x-05 padding-y-2px bg-primary-lighter radius-md text-thin text-base-dark">
+            V{item.eicr_version_number}
+          </span>
+        )}
+        <br />
+        DOB: {item.patient_date_of_birth}
+      </td>
+      <td>{item.date_created}</td>
+      <td>{item.patient_report_date || noData}</td>
+      <td>{conditionsList}</td>
+      <td>{summariesList}</td>
+    </tr>
+  );
+};
