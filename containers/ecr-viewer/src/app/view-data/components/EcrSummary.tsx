d7abd59e0129dba99870760c393f648ac3ed907e
Josh Nygaard <141273852+JNygaard-Skylight@users.noreply.github.com>
2025-02-21 09:50:07 -0500

    refactor: remove unneeded brackets.
    Co-authored-by: Mary McGrath <m.c.mcgrath13@gmail.com>

--- containers/ecr-viewer/src/app/view-data/components/EcrSummary.tsx
+++ containers/ecr-viewer/src/app/view-data/components/EcrSummary.tsx
@@ -56,7 +56,7 @@ const EcrSummary: React.FC<EcrSummaryProps> = ({
               className="text-bold margin-top-0 margin-bottom-1"
               id={"relevant-clinical"}
             >
-              {"Clinical Sections Relevant to Reportable Condition"}
+              Clinical Sections Relevant to Reportable Condition
             </h5>
             {condition.immunizationDetails.length > 0 && (
               <div className={"margin-top-0"}>
