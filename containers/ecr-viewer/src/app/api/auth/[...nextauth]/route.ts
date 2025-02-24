ca4f609c9cb8b898fe7a501e65ec09ef0e3ea7fb
Boban Ljuljdjurovic <boban@skylight.digital>
2025-02-24 09:47:37 -0500

    fix: add NEXTAUTH_URL in .env so redirects work natively

--- containers/ecr-viewer/src/app/api/auth/[...nextauth]/route.ts
+++ containers/ecr-viewer/src/app/api/auth/[...nextauth]/route.ts
@@ -24,15 +24,6 @@ const azure = () => {
 };
 const handler = NextAuth({
   providers: [keycloak(), azure()].filter((p) => p !== undefined),
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      const defaultUrl = `${baseUrl}/ecr-viewer`;
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      else if (url === baseUrl) return defaultUrl;
-      else if (new URL(url).origin === baseUrl) return url;
-      return defaultUrl;
-    },
-  },
 });
 
 export { handler as GET, handler as POST };
