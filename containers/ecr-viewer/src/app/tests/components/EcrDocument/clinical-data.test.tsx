c97f9ade8c7154d7c55a2d6cf17dd55b55146a84
Lina Roth <lina@skylight.digital>
2025-02-25 13:39:36 -0500

    Fix

--- containers/ecr-viewer/src/app/tests/components/EcrDocument/clinical-data.test.tsx
+++ containers/ecr-viewer/src/app/tests/components/EcrDocument/clinical-data.test.tsx
@@ -1,13 +1,4 @@
-import { loadYamlConfig } from "@/app/api/utils";
-import {
-  getMedicationDisplayName,
-  returnHtmlTableContent,
-} from "@/app/view-data/components/common";
-import BundleLabNoLabIds from "../assets/BundleLabNoLabIds.json";
-import { Bundle } from "fhir/r4";
-import { render, screen } from "@testing-library/react";
-
-const mappings = loadYamlConfig();
+import { getMedicationDisplayName } from "@/app/view-data/components/EcrDocument/clinical-data";
 describe("common tests", () => {
   describe("getMedicationDisplayName", () => {
     it("handles undefined case", () => {
@@ -20,50 +11,50 @@ describe("common tests", () => {
 
     it("handles single named case", () => {
       expect(
-        getMedicationDisplayName({
-          coding: [{ code: "123", display: "medication", system: "ABC" }],
-        }),
+          getMedicationDisplayName({
+            coding: [{ code: "123", display: "medication", system: "ABC" }],
+          }),
       ).toBe("medication");
     });
 
     it("handles single un-named case", () => {
       expect(
-        getMedicationDisplayName({
-          coding: [{ code: "123", system: "ABC" }],
-        }),
+          getMedicationDisplayName({
+            coding: [{ code: "123", system: "ABC" }],
+          }),
       ).toBe("Unknown medication name - ABC code 123");
     });
 
     it("handles multiple named case", () => {
       expect(
-        getMedicationDisplayName({
-          coding: [
-            { code: "123", display: "first", system: "ABC" },
-            { code: "456", display: "second", system: "DEF" },
-          ],
-        }),
+          getMedicationDisplayName({
+            coding: [
+              { code: "123", display: "first", system: "ABC" },
+              { code: "456", display: "second", system: "DEF" },
+            ],
+          }),
       ).toBe("first");
     });
 
     it("handles multiple mixed named case", () => {
       expect(
-        getMedicationDisplayName({
-          coding: [
-            { code: "123", system: "ABC" },
-            { code: "456", display: "second", system: "DEF" },
-          ],
-        }),
+          getMedicationDisplayName({
+            coding: [
+              { code: "123", system: "ABC" },
+              { code: "456", display: "second", system: "DEF" },
+            ],
+          }),
       ).toBe("second");
     });
 
     it("handles multiple un-named case", () => {
       expect(
-        getMedicationDisplayName({
-          coding: [
-            { code: "123", system: "ABC" },
-            { code: "456", system: "DEF" },
-          ],
-        }),
+          getMedicationDisplayName({
+            coding: [
+              { code: "123", system: "ABC" },
+              { code: "456", system: "DEF" },
+            ],
+          }),
       ).toBe("Unknown medication name - ABC code 123");
     });
   });
