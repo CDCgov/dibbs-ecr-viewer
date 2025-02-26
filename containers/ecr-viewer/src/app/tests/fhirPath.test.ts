2db1f51febb5810583d5e5f149ad4fd1b3ae46d4
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-02-26 12:34:49 -0500

    fix: move test file

--- containers/ecr-viewer/src/app/tests/fhirPath.test.ts
+++ containers/ecr-viewer/src/app/tests/fhirPath.test.ts
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
 import fhirPathMappings from "@/app/view-data/fhirPath";
 
-// TODO PR: where should this file live?
 describe("fhirPath", () => {
   it("returns the yaml config", () => {
     expect(Object.keys(fhirPathMappings).length).toBeGreaterThan(3);
