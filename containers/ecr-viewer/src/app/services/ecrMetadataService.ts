e2fff16ea5ef046285e40dca1b80b2be0a3a42b2
Josh Nygaard <141273852+JNygaard-Skylight@users.noreply.github.com>
2025-02-28 10:13:33 -0500

    refactor: replace error throw with console log for missing reportable triggers (#427)

--- containers/ecr-viewer/src/app/services/ecrMetadataService.ts
+++ containers/ecr-viewer/src/app/services/ecrMetadataService.ts
@@ -64,7 +64,7 @@ export const evaluateEcrMetadata = (
     }
 
     if (!triggers.size) {
-      throw new Error("No triggers found for reportable condition");
+      console.error("No triggers found for reportable condition");
     }
 
     triggers.forEach((trigger) => {
