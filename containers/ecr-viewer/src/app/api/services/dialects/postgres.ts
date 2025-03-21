db9c40694affe9c45d23ea085eb16ef216b19398
Andrew Kasper <andrew.kasper@skylight.digital>
2025-03-21 11:55:52 -0400

    fix: Support PG connection string literal

--- containers/ecr-viewer/src/app/api/services/dialects/postgres.ts
+++ containers/ecr-viewer/src/app/api/services/dialects/postgres.ts
@@ -1,21 +1,12 @@
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
-import pdu, { DatabaseConfig } from "ts-parse-database-url";
-
-const dbConfig: DatabaseConfig = pdu(process.env.DATABASE_URL || "");
 
 import { Core } from "@/app/api/services/types/core";
 import { Extended } from "@/app/api/services/types/extended";
 
 export const dialect = {
   dialect: new PostgresDialect({
-    pool: new Pool({
-      database: dbConfig.database || "ecr_viewer_db",
-      host: dbConfig.host || "localhost",
-      user: dbConfig.user,
-      password: dbConfig.password,
-      port: dbConfig.port || 5432,
-    }),
+    pool: new Pool({connectionString: process.env.DATABASE_URL}),
   }),
 };
 
