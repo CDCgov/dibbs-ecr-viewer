189ff9f84135baa59165aef0cd355dcb4fa45dde
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-03-26 12:46:09 -0400

    feat: add "DUAL" configs

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -21,30 +21,21 @@ function setupConfigurationVariables() {
   };
   process.env.SOURCE = sourceMap[process.env.CONFIG_NAME?.split("_")[0]];
 
-  switch (process.env.CONFIG_NAME) {
-    case "AWS_INTEGRATED":
-    case "AZURE_INTEGRATED":
-    case "GCP_INTEGRATED":
-      process.env.NBS_AUTH = "true";
-      break;
+  // INTEGRATED and DUAL should have NBS auth
+  if (process.env.CONFIG_NAME?.endsWith("_NON_INTEGRATED")) {
+    process.env.NBS_AUTH = "false";
+  } else if (
+    process.env.CONFIG_NAME?.endsWith("_INTEGRATED") ||
+    process.env.CONFIG_NAME?.endsWith("_DUAL")
+  ) {
+    process.env.NBS_AUTH = "true";
+  }
 
-    case "AWS_PG_NON_INTEGRATED":
-    case "AZURE_PG_NON_INTEGRATED":
-    case "GCP_PG_NON_INTEGRATED":
-      process.env.NBS_AUTH = "false";
-      process.env.METADATA_DATABASE_TYPE = "postgres";
-      process.env.METADATA_DATABASE_SCHEMA = "core";
-      break;
-
-    case "AWS_SQLSERVER_NON_INTEGRATED":
-    case "AZURE_SQLSERVER_NON_INTEGRATED":
-    case "GCP_SQLSERVER_NON_INTEGRATED":
-      process.env.NBS_AUTH = "false";
-      process.env.METADATA_DATABASE_TYPE = "sqlserver";
-      process.env.METADATA_DATABASE_SCHEMA = "extended";
-      break;
-
-    default:
-      break;
+  if (process.env.CONFIG_NAME?.includes("_PG_")) {
+    process.env.METADATA_DATABASE_TYPE = "postgres";
+    process.env.METADATA_DATABASE_SCHEMA = "core";
+  } else if (process.env.CONFIG_NAME?.includes("_SQLSERVER_")) {
+    process.env.METADATA_DATABASE_TYPE = "sqlserver";
+    process.env.METADATA_DATABASE_SCHEMA = "extended";
   }
 }
