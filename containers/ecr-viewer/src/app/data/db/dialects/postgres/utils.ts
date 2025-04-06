5fbc46ea2ad289bebcf84b8e8190327465c78eb0
Andrew Kasper <andrew.kasper@skylight.digital>
2025-04-06 13:26:55 -0400

    feat: Add utils and additional dialect-specific capabilities for database introspection

---
+++ containers/ecr-viewer/src/app/data/db/dialects/postgres/utils.ts
@@ -0,0 +1,120 @@
+import { Kysely } from 'kysely';
+
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema to check
+ * @returns true if the schema exists, false otherwise
+ */
+export async function schemaExistsByName(kysely: Kysely<any>, schemaName: string): Promise<boolean> {
+  const result = await kysely
+    .selectFrom('information_schema.schemata')
+    .select('schema_name')
+    .where('schema_name', '=', schemaName)
+    .executeTakeFirst();
+
+  return !!result; // Returns true if schema exists, false otherwise
+}
+
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema containing the table
+ * @param tableName - the name of the table to check
+ * @returns true if the table exists, false otherwise
+ */
+export async function tableExistsByName(kysely: Kysely<any>, schemaName: string, tableName: string): Promise<boolean> {
+  const result = await kysely
+    .selectFrom('information_schema.tables')
+    .select('table_name')
+    .where('table_schema', '=', schemaName)
+    .where('table_name', '=', tableName)
+    .executeTakeFirst();
+
+  return !!result; // Returns true if table exists, false otherwise
+}
+
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema containing the table
+ * @param tableName - the name of the table containing the column
+ * @param columnName - the name of the column to check
+ * @returns true if the column exists, false otherwise
+ */
+export async function columnExistsByName(kysely: Kysely<any>, schemaName: string, tableName: string, columnName: string): Promise<boolean> {
+    const result = await kysely
+        .selectFrom('information_schema.columns')
+        .select('column_name')
+        .where('table_schema', '=', schemaName)
+        .where('table_name', '=', tableName)
+        .where('column_name', '=', columnName)
+        .executeTakeFirst();
+    
+    return !!result; // Returns true if column exists, false otherwise
+}
+
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema containing the table
+ * @param tableName - the name of the table containing the column
+ * @param columnName - the name of the column to return
+ * @returns the column information if it exists, undefined otherwise
+ */
+export async function getColumn(kysely: Kysely<any>, schemaName: string, tableName: string, columnName: string) {
+  const result = await kysely
+    .selectFrom('information_schema.columns')
+    .selectAll()
+    .where('table_schema', '=', schemaName)
+    .where('table_name', '=', tableName)
+    .where('column_name', '=', columnName)
+    .executeTakeFirst();
+
+  return result;
+}
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema containing the table
+ * @param tableName - the name of the table to return
+ * @returns the table information if it exists, undefined otherwise
+ */
+export async function getTable(kysely: Kysely<any>, schemaName: string, tableName: string) {
+  const result = await kysely
+    .selectFrom('information_schema.tables')
+    .selectAll()
+    .where('table_schema', '=', schemaName)
+    .where('table_name', '=', tableName)
+    .executeTakeFirst();
+
+  return result;
+}
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @param schemaName - the name of the schema to return
+ * @returns the schema information if it exists, undefined otherwise
+ */
+export async function getSchema(kysely: Kysely<any>, schemaName: string) {
+  const result = await kysely
+    .selectFrom('information_schema.schemata')
+    .selectAll()
+    .where('schema_name', '=', schemaName)
+    .executeTakeFirst();
+
+  return result;
+}
+/**
+ *
+ * @param kysely - the Kysely instance
+ * @returns all schemas in the database
+ */
+export async function getSchemas(kysely: Kysely<any>) {
+  const result = await kysely
+    .selectFrom('information_schema.schemata')
+    .selectAll()
+    .execute();
+
+  return result;
+}
---
+++ containers/ecr-viewer/src/app/data/db/utils.ts
@@ -0,0 +1,36 @@
+import { Kysely } from 'kysely';
+
+import * as postgresUtils from './dialects/postgres/utils';
+import * as sqlServerUtils from './dialects/sqlserver/utils';
+
+// Define a type for the utils object to ensure type safety
+export type DbUtils = {
+  schemaExistsByName: (kysely: Kysely<any>, schemaName: string) => Promise<boolean>;
+  tableExistsByName: (kysely: Kysely<any>, schemaName: string, tableName: string) => Promise<boolean>;
+  columnExistsByName: (kysely: Kysely<any>, schemaName: string, tableName: string, columnName: string) => Promise<boolean>;
+  getColumn: (kysely: Kysely<any>, schemaName: string, tableName: string, columnName: string) => Promise<any>;
+  getTable: (kysely: Kysely<any>, schemaName: string, tableName: string) => Promise<any>;
+  getSchema: (kysely: Kysely<any>, schemaName: string) => Promise<any>;
+  getSchemas: (kysely: Kysely<any>) => Promise<any[]>;
+};
+
+// Select the appropriate utils based on the dialect
+/**
+ * @returns - The database utility functions for the specified dialect.
+ * @throws {Error} - If the dialect is not supported or not set.
+ */
+export function getDbUtils(): DbUtils {
+  const dialect = process.env.DB_DIALECT;
+
+  if (!dialect) {
+      throw new Error('DB_DIALECT environment variable is not set');
+  }
+
+  if (dialect === 'postgres') {
+    return postgresUtils;
+  } else if (dialect === 'sqlserver') {
+    return sqlServerUtils;
+  } else {
+    throw new Error(`Unsupported dialect: ${dialect.constructor.name}`);
+  }
+}
