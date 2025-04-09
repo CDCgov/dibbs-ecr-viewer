758203bc730e86d0ea8866df263edb6626d11cb7
Austin Hall <austin.hall@skylight.digital>
2025-04-09 12:24:52 -0400

    change viewer url

--- containers/ecr-viewer/seed-scripts/create-seed-data.py
+++ containers/ecr-viewer/seed-scripts/create-seed-data.py
@@ -5,7 +5,7 @@ import zipfile
 
 import grequests
 
-UPLOAD_URL = "http://host.docker.internal:3000/ecr-viewer/api/process-zip"
+UPLOAD_URL = "http://ecr-viewer:3000/ecr-viewer/api/process-zip"
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
 
