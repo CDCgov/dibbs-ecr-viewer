ef8a60cb98a6c551859cc7397c3157b085c7cb62
Andrew Kasper <andrew.kasper@skylight.digital>
2025-04-09 09:08:43 -0400

    feat: fail to launch if there are pending migrations

--- containers/ecr-viewer/src/instrumentation.ts
+++ containers/ecr-viewer/src/instrumentation.ts
@@ -1,4 +1,5 @@
 import { AZURE_SOURCE, GCP_SOURCE, S3_SOURCE } from "./app/api/utils";
+import { hasPendingMigrations } from "./app/data/db/utils/migrate";
 
 /**
  * The register function will be callled once when nextjs server is instantiated
@@ -8,6 +9,13 @@ export async function register() {
 
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./app/services/instrumentation");
+  }
+
+  if (await hasPendingMigrations()) {
+    console.error(
+      "Pending migrations detected. Please run the migration command before starting the server.",
+    );
+    process.exit(1);
   }
 }
 
