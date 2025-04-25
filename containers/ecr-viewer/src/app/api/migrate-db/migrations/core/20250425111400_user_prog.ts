5f650a1fbd0e684dd53dba3ec0324ae148c0f01b
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-04-25 12:34:59 -0400

    fix: cleanup

--- containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250425111400_user_prog.ts
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250425111400_user_prog.ts
@@ -16,53 +16,51 @@ export async function up(db: Kysely<AnyDb>): Promise<void> {
     (table) => tables.includes(table),
   );
 
-  if (!tablesExist) {
-    const _db = db.withSchema(schema);
+  // nothing to do here
+  if (tablesExist) return;
 
-    await _db.schema
-      .createTable("user")
-      .addColumn("uuid", "varchar(200)", (cb) => cb.primaryKey())
-      .addColumn("email", "varchar(200)", (cb) => cb.notNull().unique())
-      .addColumn("name", "varchar(200)")
-      .addColumn("date_of_last_login", getSql("datetimeTzType"))
-      .addColumn("user_type", "varchar(12)", (cb) => cb.notNull())
-      .addColumn("status", "varchar(12)", (cb) =>
-        cb.notNull().defaultTo("active"),
-      )
-      .addColumn("date_created", getSql("datetimeTzType"), (cb) =>
-        cb.notNull().defaultTo(getSql("now")),
-      )
-      .addColumn("author_uuid", "varchar(200)", (cb) =>
-        cb.notNull().references("user.uuid"),
-      )
-      .execute();
+  const _db = db.withSchema(schema);
 
-    await _db.schema
-      .createTable("program_area")
-      .addColumn("uuid", "varchar(200)", (cb) => cb.primaryKey())
-      .addColumn("name", "varchar(200)", (cb) => cb.notNull().unique())
-      .addColumn("date_created", getSql("datetimeTzType"), (cb) =>
-        cb.notNull().defaultTo(getSql("now")),
-      )
-      .addColumn("author_uuid", "varchar(200)", (cb) =>
-        cb.notNull().references("user.uuid"),
-      )
-      .execute();
+  await _db.schema
+    .createTable("user")
+    .addColumn("uuid", "varchar(200)", (cb) => cb.primaryKey())
+    .addColumn("email", "varchar(200)", (cb) => cb.notNull().unique())
+    .addColumn("name", "varchar(200)")
+    .addColumn("date_of_last_login", getSql("datetimeTzType"))
+    .addColumn("user_type", "varchar(12)", (cb) => cb.notNull())
+    .addColumn("status", "varchar(12)", (cb) =>
+      cb.notNull().defaultTo("active"),
+    )
+    .addColumn("date_created", getSql("datetimeTzType"), (cb) =>
+      cb.notNull().defaultTo(getSql("now")),
+    )
+    .addColumn("author_uuid", "varchar(200)", (cb) =>
+      cb.notNull().references("user.uuid"),
+    )
+    .execute();
 
-    await _db.schema
-      .createTable("user_program_area")
-      .addColumn("user_uuid", "varchar(200)", (cb) =>
-        cb.notNull().references("user.uuid"),
-      )
-      .addColumn("program_area_uuid", "varchar(200)", (cb) =>
-        cb.notNull().references("program_area.uuid"),
-      )
-      .addPrimaryKeyConstraint("primary_key", [
-        "user_uuid",
-        "program_area_uuid",
-      ])
-      .execute();
-  }
+  await _db.schema
+    .createTable("program_area")
+    .addColumn("uuid", "varchar(200)", (cb) => cb.primaryKey())
+    .addColumn("name", "varchar(200)", (cb) => cb.notNull().unique())
+    .addColumn("date_created", getSql("datetimeTzType"), (cb) =>
+      cb.notNull().defaultTo(getSql("now")),
+    )
+    .addColumn("author_uuid", "varchar(200)", (cb) =>
+      cb.notNull().references("user.uuid"),
+    )
+    .execute();
+
+  await _db.schema
+    .createTable("user_program_area")
+    .addColumn("user_uuid", "varchar(200)", (cb) =>
+      cb.notNull().references("user.uuid"),
+    )
+    .addColumn("program_area_uuid", "varchar(200)", (cb) =>
+      cb.notNull().references("program_area.uuid"),
+    )
+    .addPrimaryKeyConstraint("primary_key", ["user_uuid", "program_area_uuid"])
+    .execute();
 }
 
 /**
