79b36872a6d84ad530a9219de8df00861283eb89
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-04-29 14:34:22 -0400

    docs: words

--- containers/ecr-viewer/src/app/services/userService.ts
+++ containers/ecr-viewer/src/app/services/userService.ts
@@ -126,7 +126,7 @@ export const listUsers = async (): Promise<User[]> => {
 /**
  * Update a user with the the given email.
  * @param email (current) email of the user to update
- * @param updates mutable fields to update in their record. UUID fields should not be updated.
+ * @param updates objecct with fields to update in their record. UUID fields should not be updated.
  */
 export const updateUserByEmail = async (
   email: string,
