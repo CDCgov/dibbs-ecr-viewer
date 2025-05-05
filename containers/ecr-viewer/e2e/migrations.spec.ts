82ec57cdbf65ea0e4a8fd2b1fdb1eb24aa9392a9
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-05 18:03:50 -0400

    test: try to use nbs auth with migration e2e

--- containers/ecr-viewer/e2e/migrations.spec.ts
+++ containers/ecr-viewer/e2e/migrations.spec.ts
@@ -15,6 +15,8 @@ const toForm = (obj: Record<string, string>) => {
   return form;
 };
 
+const NBS_AUTH_QUERY = `?auth=eyJhbGciOiJSUzI1NiIsImlkIjoiYmxhaCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.hXmX6wu9ThiSqNEl6Y3pBETppiIt0j4RKSVPO_AAYZJZsngSFiu8GuGDtA13kJ-texfUHshqcy4euoVwfmN-naDi2Ly6p6lPjY6xzmTuQ1DtiKLZDNBsDupjoLAuIJQ3K8uWRnCdRGG1ZlTkZa-SG8b4jfDLRrl1fPiJCWM62XV7_gIvqCvRAPdP9kMrOV1LtLEuXgoXZGifVNnPQhtT7fQ7kDmbM-HDG4MquZy89CIRy2q22xIclePOAoe0Ifz6q7-NG3I9CzKOAa_Vx6Oy5ZYBYphfV1n46gp4OC0Cb_w-wFLfRDuDPJZvcS5ed2HxdyZrU_GeD4WSN5IQpEn_45CZifBzmv9-jweEUD2or3sp1DReORLZG2CvBqtixC0p3gIeGnY4HROduafmDfyI0gcv7pDM-fcreMCBG-7uqUPkk9rqhCPw9n6fhWvNMSGrtW9tx6hAPNxjKJ2AsyTh7cJyR0teVpijhXZz0dGJOtYY1-nlR7_BnJH2lC9tLiIJcVl1JKfGRu18MV1bHs7y25Wp1HxVDUXllShXa7_oD7ljnE3stmpO5GPMbxvWC_RKO_bu_e2mAgJ3yiPImFpLVYZZgBqClctciZMQeV1lZTAy-7Xlzgdx-IvFc9VuigKw6hfk4on98BxMUENeh20KIgVv8cMr4ZjAGV3MjnFnHWw`;
+
 test.describe("migrations", () => {
   test.beforeEach(logInToKeycloak);
 
@@ -29,7 +31,9 @@ test.describe("migrations", () => {
     ).toBeVisible();
     await expect(page.getByText("eCR Library")).not.toBeVisible();
 
-    const noSecret = await request.post(`/ecr-viewer/api/migrate-db`);
+    const noSecret = await request.post(
+      `/ecr-viewer/api/migrate-db${NBS_AUTH_QUERY}`,
+    );
     expect(await noSecret.json()).toEqual(
       expect.objectContaining({
         message: "Validation error",
@@ -37,9 +41,12 @@ test.describe("migrations", () => {
     );
     expect(noSecret.ok()).toBeFalsy();
 
-    const wrongSecret = await request.post(`/ecr-viewer/api/migrate-db`, {
-      form: toForm({ migration_secret: "nope" }),
-    });
+    const wrongSecret = await request.post(
+      `/ecr-viewer/api/migrate-db${NBS_AUTH_QUERY}`,
+      {
+        form: toForm({ migration_secret: "nope" }),
+      },
+    );
     expect(await wrongSecret.json()).toEqual(
       expect.objectContaining({
         message:
@@ -48,9 +55,12 @@ test.describe("migrations", () => {
     );
     expect(wrongSecret.ok()).toBeFalsy();
 
-    const up = await request.post(`/ecr-viewer/api/migrate-db`, {
-      form: toForm({ migration_secret: "test" }),
-    });
+    const up = await request.post(
+      `/ecr-viewer/api/migrate-db${NBS_AUTH_QUERY}`,
+      {
+        form: toForm({ migration_secret: "test" }),
+      },
+    );
     expect(await up.json()).toEqual(
       expect.objectContaining({ message: "success" }),
     );
@@ -62,9 +72,12 @@ test.describe("migrations", () => {
     ).not.toBeVisible();
     await expect(page.getByText("eCR Library")).toBeVisible();
 
-    const down = await request.post(`/ecr-viewer/api/migrate-db`, {
-      form: toForm({ migration_secret: "test", direction: "down" }),
-    });
+    const down = await request.post(
+      `/ecr-viewer/api/migrate-db${NBS_AUTH_QUERY}`,
+      {
+        form: toForm({ migration_secret: "test", direction: "down" }),
+      },
+    );
     expect(await down.json()).toEqual(
       expect.objectContaining({ message: "success" }),
     );
