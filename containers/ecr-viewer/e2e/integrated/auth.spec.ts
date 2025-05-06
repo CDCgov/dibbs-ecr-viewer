a5cb849c4751c8bd0b7ca7f1535db7713ff81f92
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-06 17:31:55 -0400

    test: fix locator

--- containers/ecr-viewer/e2e/integrated/auth.spec.ts
+++ containers/ecr-viewer/e2e/integrated/auth.spec.ts
@@ -3,10 +3,12 @@ import { test, expect } from "@playwright/test";
 // all integrated tests should start with 'integrated -' in order to match succesfully in npm run test:e2e:integrated
 // test:e2e:integrated is only required since CONFIG_NAME needs to be changed. This can be removed when dual boot (nbs auth & idp auth) is supported
 test.describe("integrated - nbs auth", () => {
-  test("should redirect to unauthorized page", async ({ page }) => {
+  test("should redirect to not found when no token", async ({ page }) => {
     await page.goto("/ecr-viewer/view-data");
 
-    await expect(page.getByText("Authentication Failed")).toBeVisible();
+    await expect(
+      page.getByText("The requested page could not be found"),
+    ).toBeVisible();
   });
   test("should show unauthorized when invalid token is provided", async ({
     page,
