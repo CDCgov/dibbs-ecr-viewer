d53967747c5b2315d52c3d5559527912eb26f1d8
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-06 11:51:11 -0400

    fix: switch over to uuid as key for RUD and add program management assignment helpers

--- containers/ecr-viewer/src/app/services/userService.ts
+++ containers/ecr-viewer/src/app/services/userService.ts
@@ -6,6 +6,7 @@ import { getDb } from "@/app/data/metadataDb/database";
 import {
   Core,
   NewUser,
+  ProgramArea,
   User,
   UserUpdate,
 } from "@/app/data/metadataDb/types/core";
@@ -105,7 +106,7 @@ const createUserQuery = async (
     if (user.status === "active") {
       throw new Error("User already exists and is active");
     } else {
-      await updateUserQuery(email, { status: "active", user_type });
+      await updateUserQuery(user.uuid, { status: "active", user_type });
       return user.uuid;
     }
   }
@@ -122,12 +123,12 @@ const createUserQuery = async (
 };
 
 /**
- * Update a user with the the given email.
- * @param email (current) email of the user to update
+ * Update a user with the the given id.
+ * @param uuid id of the user to update
  * @param updates objecct with fields to update in their record. UUID fields should not be updated.
  */
-export const updateUserByEmail = async (
-  email: string,
+export const updateUser = async (
+  uuid: string,
   updates: Omit<UserUpdate, "uuid" | "author_uuid">,
 ): Promise<void> => {
   const updatingUser = await getLoggedInUser();
@@ -136,7 +137,7 @@ export const updateUserByEmail = async (
   }
 
   try {
-    await updateUserQuery(email, updates);
+    await updateUserQuery(uuid, updates);
   } catch (error: unknown) {
     const message = "Failed to update user";
     console.error({ message, error });
@@ -145,29 +146,91 @@ export const updateUserByEmail = async (
 };
 
 const updateUserQuery = async (
-  email: string,
+  uuid: string,
   updates: Omit<UserUpdate, "uuid" | "author_uuid">,
 ) => {
   await getDb<Core>()
     .updateTable("user")
     .set(updates)
-    .where("email", "=", email)
+    .where("uuid", "=", uuid)
     .execute();
 };
 
+// TODO PR: should this be admin limited? can a user know their own program areas?
 /**
- * Delete user with the given email. The deleting user must be an admin. A
- * user can indeed delete themselves.
- * @param email Email of the user to delete
+ * List the program areas a user is assigned to.
+ * @param uuid id of the user to update
+ * @returns list of program areas
  */
-export const deleteUser = async (email: string): Promise<void> => {
+export const listUserProgramAreas = async (
+  uuid: string,
+): Promise<ProgramArea[]> => {
+  try {
+    return await getDb<Core>()
+      .selectFrom(["user_program_area", "program_area"])
+      .selectAll(["program_area"])
+      .where("user_uuid", "=", uuid)
+      .where(({ eb, ref }) =>
+        eb("user_program_area.program_area_uuid", "=", ref("uuid")),
+      )
+      .execute();
+  } catch (error: unknown) {
+    const message = "Failed to list user program areas";
+    console.error({ message, error });
+    throw new Error(message);
+  }
+};
+
+/**
+ * Update a user with the the given id's program areas to the given set.
+ * @param uuid id of the user to update
+ * @param programAreaUuids UUIDs of program areas the user is assigned to.
+ */
+export const updateUserProgramAreas = async (
+  uuid: string,
+  programAreaUuids: string[],
+): Promise<void> => {
+  const updatingUser = await getLoggedInUser();
+  if (!isAdmin(updatingUser)) {
+    throw new Error("Standard user cannot update users");
+  }
+
+  try {
+    await getDb<Core>()
+      .transaction()
+      .execute(async (db) => {
+        await db
+          .deleteFrom("user_program_area")
+          .where("user_uuid", "=", uuid)
+          .execute();
+        for (const program_area_uuid of programAreaUuids) {
+          await db
+            .insertInto("user_program_area")
+            .values({ user_uuid: uuid, program_area_uuid })
+            .execute();
+        }
+      });
+  } catch (error: unknown) {
+    const message = "Failed to update user";
+    console.error({ message, error });
+    throw new Error(message);
+  }
+};
+
+/**
+ * Delete user with the given id. The deleting user must be an admin. A
+ * user can indeed delete themselves.
+ * @param uuid Email of the user to delete
+ */
+export const deleteUser = async (uuid: string): Promise<void> => {
   const deletingUser = await getLoggedInUser();
   if (!isAdmin(deletingUser)) {
     throw new Error("Standard user cannot delete users");
   }
 
   try {
-    await updateUserQuery(email, { status: "deleted" });
+    await updateUserQuery(uuid, { status: "deleted" });
+    // TODO: delete program area assignments once that crud is added
   } catch (error: unknown) {
     const message = "Failed to delete user";
     console.error({ message, error });
