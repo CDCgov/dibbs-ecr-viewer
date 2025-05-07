ba5e5125c929742cfe2715c3c576352b062f6ebb
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-07 14:17:43 -0400

    test: add e2e tests

--- containers/ecr-viewer/e2e/dual/utils.ts
+++ containers/ecr-viewer/e2e/dual/utils.ts
@@ -1,4 +1,8 @@
-import { PlaywrightTestArgs, expect } from "@playwright/test";
+import {
+  APIRequestContext,
+  PlaywrightTestArgs,
+  expect,
+} from "@playwright/test";
 
 /**
  * Helper to lot into via keycloak and go to the viewer page
@@ -18,4 +22,25 @@ export const logInToKeycloak = async ({ page }: PlaywrightTestArgs) => {
   await page.getByRole("button", { name: "Sign in" }).click();
 
   expect(page.getByText("eCR Library"));
+};
+
+/**
+ * Get an auth token from keycloak
+ * @param request api request context from test
+ * @returns token
+ */
+export const getKeycloakToken = async (request: APIRequestContext) => {
+  const form = new FormData();
+  form.append("client_id", process.env.AUTH_CLIENT_ID!);
+  form.append("client_secret", process.env.AUTH_CLIENT_SECRET!);
+  form.append("username", process.env.AUTH_USER!);
+  form.append("password", process.env.AUTH_PASSWORD!);
+  form.append("grant_type", "password");
+  form.append("scope", "openid email profile");
+  const resp = await request.post(
+    `${process.env.AUTH_ISSUER}/protocol/openid-connect/token`,
+    { form },
+  );
+  const body = await resp.json();
+  return body.access_token;
 };
--- containers/ecr-viewer/e2e/integrated/auth.spec.ts
+++ containers/ecr-viewer/e2e/integrated/auth.spec.ts
@@ -46,4 +46,52 @@ test.describe("integrated - nbs auth", () => {
       "http://localhost:3000/ecr-viewer/error/notfound",
     );
   });
+
+  test("should authenticate on api route if NBS auth token header provided", async ({
+    request,
+  }) => {
+    const resp = await request.post(`/ecr-viewer/api/migrate-db`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJSUzI1NiIsImlkIjoiYmxhaCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.hXmX6wu9ThiSqNEl6Y3pBETppiIt0j4RKSVPO_AAYZJZsngSFiu8GuGDtA13kJ-texfUHshqcy4euoVwfmN-naDi2Ly6p6lPjY6xzmTuQ1DtiKLZDNBsDupjoLAuIJQ3K8uWRnCdRGG1ZlTkZa-SG8b4jfDLRrl1fPiJCWM62XV7_gIvqCvRAPdP9kMrOV1LtLEuXgoXZGifVNnPQhtT7fQ7kDmbM-HDG4MquZy89CIRy2q22xIclePOAoe0Ifz6q7-NG3I9CzKOAa_Vx6Oy5ZYBYphfV1n46gp4OC0Cb_w-wFLfRDuDPJZvcS5ed2HxdyZrU_GeD4WSN5IQpEn_45CZifBzmv9-jweEUD2or3sp1DReORLZG2CvBqtixC0p3gIeGnY4HROduafmDfyI0gcv7pDM-fcreMCBG-7uqUPkk9rqhCPw9n6fhWvNMSGrtW9tx6hAPNxjKJ2AsyTh7cJyR0teVpijhXZz0dGJOtYY1-nlR7_BnJH2lC9tLiIJcVl1JKfGRu18MV1bHs7y25Wp1HxVDUXllShXa7_oD7ljnE3stmpO5GPMbxvWC_RKO_bu_e2mAgJ3yiPImFpLVYZZgBqClctciZMQeV1lZTAy-7Xlzgdx-IvFc9VuigKw6hfk4on98BxMUENeh20KIgVv8cMr4ZjAGV3MjnFnHWw",
+      },
+    });
+
+    // This means we got past auth and failed on a bad migration call - this is good
+    expect(await resp.json()).toEqual(
+      expect.objectContaining({
+        message: "Validation error",
+      }),
+    );
+    expect(resp.ok()).toBeFalsy();
+  });
+
+  test("should not authenticate on api route if NBS auth token search param provided", async ({
+    request,
+  }) => {
+    const resp = await request.post(
+      `/ecr-viewer/api/migrate-db?auth=eyJhbGciOiJSUzI1NiIsImlkIjoiYmxhaCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.hXmX6wu9ThiSqNEl6Y3pBETppiIt0j4RKSVPO_AAYZJZsngSFiu8GuGDtA13kJ-texfUHshqcy4euoVwfmN-naDi2Ly6p6lPjY6xzmTuQ1DtiKLZDNBsDupjoLAuIJQ3K8uWRnCdRGG1ZlTkZa-SG8b4jfDLRrl1fPiJCWM62XV7_gIvqCvRAPdP9kMrOV1LtLEuXgoXZGifVNnPQhtT7fQ7kDmbM-HDG4MquZy89CIRy2q22xIclePOAoe0Ifz6q7-NG3I9CzKOAa_Vx6Oy5ZYBYphfV1n46gp4OC0Cb_w-wFLfRDuDPJZvcS5ed2HxdyZrU_GeD4WSN5IQpEn_45CZifBzmv9-jweEUD2or3sp1DReORLZG2CvBqtixC0p3gIeGnY4HROduafmDfyI0gcv7pDM-fcreMCBG-7uqUPkk9rqhCPw9n6fhWvNMSGrtW9tx6hAPNxjKJ2AsyTh7cJyR0teVpijhXZz0dGJOtYY1-nlR7_BnJH2lC9tLiIJcVl1JKfGRu18MV1bHs7y25Wp1HxVDUXllShXa7_oD7ljnE3stmpO5GPMbxvWC_RKO_bu_e2mAgJ3yiPImFpLVYZZgBqClctciZMQeV1lZTAy-7Xlzgdx-IvFc9VuigKw6hfk4on98BxMUENeh20KIgVv8cMr4ZjAGV3MjnFnHWw`,
+    );
+
+    expect(await resp.json()).toEqual(
+      expect.objectContaining({
+        message: "API uses token authentication",
+      }),
+    );
+    expect(resp.ok()).toBeFalsy();
+  });
+
+  test("should not authenticate on api route if bad auth token header provided", async ({
+    request,
+  }) => {
+    const resp = await request.post(`/ecr-viewer/api/migrate-db`, {
+      headers: { Authorization: "Bearer jibberish.token.please" },
+    });
+    expect(await resp.json()).toEqual(
+      expect.objectContaining({
+        message: "API uses token authentication",
+      }),
+    );
+    expect(resp.ok()).toBeFalsy();
+  });
 });
