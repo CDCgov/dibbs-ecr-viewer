8d69187810d95dafda64606acdb4a20a3a7fcfb4
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-16 16:46:13 -0400

    feat: wip - program details pane

--- containers/ecr-viewer/src/app/admin/program/ProgramTable.tsx
+++ containers/ecr-viewer/src/app/admin/program/ProgramTable.tsx
@@ -1,27 +1,18 @@
 "use client";
+import { useState } from "react";
+
+import {
+  DetailsSidePanel,
+  DetailsTrigger,
+  useDetailsRef,
+} from "@/app/components/DetailsSidePanel";
 import {
   PaginatedSortableTable,
   TableColumn,
 } from "@/app/components/table/PaginatedSortableTable";
 import { ConditionReference } from "@/app/data/metadataDb/types/core";
+import { formatDateTime } from "@/app/services/formatDateService";
 import { ListedProgramArea } from "@/app/services/programAreaService";
-
-const tableHeaders: TableColumn<ListedProgramArea>[] = [
-  {
-    id: "name",
-    value: "Program Area",
-    dataSortable: true,
-    sortDirection: "ASC",
-  },
-  {
-    id: "conditions",
-    value: "Number of Conditions",
-    dataSortable: false,
-    sortDirection: "",
-    formatter: (conditions: ConditionReference[]) =>
-      `${conditions.length} condition${conditions.length === 1 ? "" : "s"}`,
-  },
-];
 
 /**
  *
@@ -34,11 +25,73 @@ export const ProgramTable = ({
 }: {
   programAreas: ListedProgramArea[];
 }) => {
+  const detailsRef = useDetailsRef();
+  const [selectedProgramArea, setSelectedProgramArea] =
+    useState<ListedProgramArea | null>(null);
+
+  const tableHeaders: TableColumn<ListedProgramArea>[] = [
+    {
+      id: "name",
+      value: "Program Area",
+      dataSortable: true,
+      sortDirection: "ASC",
+      formatter: (v: string, programArea: ListedProgramArea) => (
+        <DetailsTrigger
+          detailsRef={detailsRef}
+          onClick={() => {
+            setSelectedProgramArea(programArea);
+          }}
+        >
+          {v}
+        </DetailsTrigger>
+      ),
+    },
+    {
+      id: "conditions",
+      value: "Number of Conditions",
+      dataSortable: false,
+      sortDirection: "",
+      formatter: (conditions: ConditionReference[]) =>
+        `${conditions.length} condition${conditions.length === 1 ? "" : "s"}`,
+    },
+  ];
   return (
-    <PaginatedSortableTable
-      initHeaders={tableHeaders}
-      items={programAreas}
-      itemType="Program areas"
-    />
+    <div>
+      <DetailsSidePanel
+        detailsRef={detailsRef}
+        title={selectedProgramArea?.name!}
+        subtitle={`Created on ${formatDateTime(
+          selectedProgramArea?.date_created.toISOString(),
+        )}`}
+        description="Program Area Information"
+        details={[
+          {
+            title: "Name",
+            value: selectedProgramArea?.name,
+          },
+          {
+            title: "Conditions",
+            value:
+              selectedProgramArea?.conditions.length === 0 ? (
+                "No conditions assigned"
+              ) : (
+                // TODO: style this!
+                <ul>
+                  {selectedProgramArea?.conditions
+                    .sort()
+                    .map(({ condition_name, code }) => (
+                      <li key={code}>{condition_name}</li>
+                    ))}
+                </ul>
+              ),
+          },
+        ]}
+      />
+      <PaginatedSortableTable
+        initHeaders={tableHeaders}
+        items={programAreas}
+        itemType="Program areas"
+      />
+    </div>
   );
 };
