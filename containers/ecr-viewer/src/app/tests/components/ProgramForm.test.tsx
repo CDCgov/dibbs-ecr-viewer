7d3e5ab2318bd9180cae18241ebbf03a11f6d413
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-05-20 09:48:38 -0400

    test: unit tests

--- containers/ecr-viewer/src/app/tests/components/ProgramForm.test.tsx
+++ containers/ecr-viewer/src/app/tests/components/ProgramForm.test.tsx
@@ -56,6 +56,7 @@ describe("ProgramForm", () => {
     });
     expect(submitButtons).toHaveLength(2);
     expect(submitButtons[0]).toBeDisabled();
+    expect(screen.getByText("Condition in A Program Area")).toBeVisible();
 
     const user = userEvent.setup();
 
@@ -78,13 +79,38 @@ describe("ProgramForm", () => {
       name: "Deselect all",
     });
     expect(deselectButtons).toHaveLength(2);
-    expect(deselectButtons[0]).toBeDisabled();
-
-    // select all in first cateogry
-    await user.click(selectButtons[0]);
-    expect(submitButtons[0]).not.toBeDisabled();
-    expect(selectButtons[0]).toBeDisabled();
     expect(deselectButtons[0]).not.toBeDisabled();
+
+    // select all in first cateogry and cancel
+    await user.click(selectButtons[0]);
+    expect(
+      screen.getByText(
+        "Are you sure you want to add all conditions from first category?",
+      ),
+    ).toBeVisible();
+    await user.click(screen.getByRole("button", { name: "Cancel" }));
+    for (const checkbox of checkboxes) {
+      expect(checkbox).not.toBeChecked();
+    }
+
+    // select the unassigned in first cateogry
+    await user.click(screen.getByRole("checkbox", { name: "condition 1" }));
+    expect(submitButtons[0]).not.toBeDisabled();
+    expect(checkboxes[0]).toBeChecked();
+    expect(checkboxes[1]).not.toBeChecked();
+    expect(checkboxes[2]).not.toBeChecked();
+
+    // select all in first cateogry and continue
+    await user.click(selectButtons[0]);
+    expect(
+      screen.getByText(
+        "Are you sure you want to add all conditions from first category?",
+      ),
+    ).toBeVisible();
+    await user.click(
+      screen.getByRole("button", { name: "Yes, add all conditions" }),
+    );
+    expect(submitButtons[0]).not.toBeDisabled();
     expect(checkboxes[0]).toBeChecked();
     expect(checkboxes[1]).toBeChecked();
     expect(checkboxes[2]).not.toBeChecked();
@@ -92,8 +118,6 @@ describe("ProgramForm", () => {
     // deselect all in first category
     await user.click(deselectButtons[0]);
     expect(submitButtons[0]).toBeDisabled();
-    expect(selectButtons[0]).not.toBeDisabled();
-    expect(deselectButtons[0]).toBeDisabled();
     expect(checkboxes[0]).not.toBeChecked();
     expect(checkboxes[1]).not.toBeChecked();
     expect(checkboxes[2]).not.toBeChecked();
@@ -103,6 +127,7 @@ describe("ProgramForm", () => {
     render(
       <ProgramForm
         action="Create"
+        progUuid="789"
         initValues={{
           name: "I have a name",
           conditions: [
@@ -143,12 +168,32 @@ describe("ProgramForm", () => {
     });
     expect(submitButtons).toHaveLength(2);
     expect(submitButtons[0]).toBeEnabled();
+    expect(
+      screen.queryByText("Condition in A Program Area"),
+    ).not.toBeInTheDocument();
 
     const nameInput = screen.getByLabelText("Program area name*");
     expect(nameInput).toHaveValue("I have a name");
 
     const checkboxes = screen.getAllByRole("checkbox");
     expect(checkboxes[0]).not.toBeChecked();
+    expect(checkboxes[1]).toBeChecked();
+    expect(checkboxes[2]).not.toBeChecked();
+
+    const user = userEvent.setup();
+
+    // deselect the currently assigned condition
+    await user.click(screen.getByRole("checkbox", { name: "condition 2" }));
+    expect(submitButtons[0]).toBeDisabled();
+    expect(checkboxes[0]).not.toBeChecked();
+    expect(checkboxes[1]).not.toBeChecked();
+    expect(checkboxes[2]).not.toBeChecked();
+
+    // select all in first category
+    const selectButtons = screen.getAllByRole("button", { name: "Select all" });
+    await user.click(selectButtons[0]);
+    expect(submitButtons[0]).not.toBeDisabled();
+    expect(checkboxes[0]).toBeChecked();
     expect(checkboxes[1]).toBeChecked();
     expect(checkboxes[2]).not.toBeChecked();
   });
