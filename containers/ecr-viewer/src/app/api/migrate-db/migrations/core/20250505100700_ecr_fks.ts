423d73f696d14990bff309ad3ba5c3787ad294f3
Andrew Kasper <andrew.kasper@skylight.digital>
2025-05-21 13:35:06 -0400

    fix: Undo naming

--- containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250505100700_ecr_fks.ts
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250505100700_ecr_fks.ts
@@ -4,7 +4,7 @@ import { AnyDb } from "@/app/data/metadataDb/database";
 import { dbNamespace } from "@/app/data/metadataDb/utils/db-config";
 
 /**
- * fks to core schema.
+ * Add fks to core schema.
  * @param db - the database connection
  */
 export async function up(db: Kysely<AnyDb>): Promise<void> {
@@ -39,7 +39,7 @@ export async function up(db: Kysely<AnyDb>): Promise<void> {
 }
 
 /**
- * Roll back fks to core schema.
+ * Remove fks from core schema
  * @param db - the database connection
  */
 export async function down(db: Kysely<AnyDb>): Promise<void> {
