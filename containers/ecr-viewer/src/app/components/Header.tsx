c0fdadad2d4f1b66994424a1fca4e597b4b4d9fe
Gordon Farrell <gordon@skylight.digital>
2025-05-21 15:39:06 -0600

    header mostly good, working dropdown user menu

--- containers/ecr-viewer/src/app/components/Header.tsx
+++ containers/ecr-viewer/src/app/components/Header.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+'use client';
 
+import React, {useEffect, useRef, useState } from "react";
+
+import Image from 'next/image';
 import Link from "next/link";
-
-import { SignOutButton } from "./SignOutButton";
 
 /**
  * Header component for the ECR Viewer project.
@@ -11,24 +12,68 @@ import { SignOutButton } from "./SignOutButton";
  * classes for styling.
  * @returns The header section of the application.
  */
-const Header: React.FC = () => (
-  <header className="usa-header usa-header--basic">
+const Header: React.FC = () =>{
+    const [showMenu, setShowMenu] = useState(true);
+    const menuRef = useRef<HTMLDivElement>(null);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const toggleMenuDropdown = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const outsideMenuClick = (event: MouseEvent) => {
+        if (
+            showMenu &&
+            menuRef.current &&
+            !menuRef.current.contains(event.target as Node) &&
+            buttonRef.current &&
+            !buttonRef.current.contains(event.target as Node)
+        ) {
+            setShowMenu(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", outsideMenuClick);
+        return () => {
+            document.removeEventListener("mousedown", outsideMenuClick);
+        };
+    }, [showMenu]);
+
+    return(<header className="usa-header usa-header--basic">
     <div
-      className="usa-nav-container flex-row flex-align-center flex-justify"
+      className="usa-nav-container"
       style={{ maxWidth: "none" }}
     >
-      <div className="usa-navbar">
-        <div className="usa-logo">
-          <h1>
-            <Link href="/" prefetch={false}>
-              eCR Viewer
-            </Link>
-          </h1>
-        </div>
-      </div>
-      <SignOutButton />
+        <Link href="/" prefetch={false}>
+            <Image src="/ecr-viewer/ecr-viewer-logo.png" alt="eCR Viewer Logo" width={185} height={40} />
+        </Link>
+        <div className="usa-nav display-flex flex-row ">
+            <ul className="usa-nav__primary usa-accordion">
+                <li className="usa-nav__primary-item">
+                    <a href="/" className="usa-nav__link">eCR Library</a>
+                </li>
+                <li className="usa-nav__primary-item">
+                    <a href="/about" className="usa-nav__link">User Management</a>
+                </li>
+                <li className="usa-nav__primary-item">
+                    <a href="/services" className="usa-nav__link">Program Management</a>
+                </li>
+            </ul>
+            <div className="user-menu-container">
+            <button ref={buttonRef} className='user-menu-button' onClick={()=> {toggleMenuDropdown()}}>
+                <Image src="/ecr-viewer/user-profile.png" alt="eCR Viewer Logo" width={28} height={28}/>
+            </button>
+            { showMenu && (
+                <div ref={menuRef} className="user-menu">
+
+                </div>
+            )}
+            </div>
+        </div >
     </div>
   </header>
-);
+)
+};
 
 export default Header;
