1cbd10c2bd3cd13dadad2dfa81536fe14648e6e8
angelathe <angela.the@skylight.digital>
2025-05-29 11:00:33 -0400

    add e2e tests

--- containers/ecr-viewer/e2e/dual/admin-user.spec.ts
+++ containers/ecr-viewer/e2e/dual/admin-user.spec.ts
@@ -1,5 +1,7 @@
 import AxeBuilder from "@axe-core/playwright";
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
+
+import { toKebabCase } from "@/app/utils/format-utils";
 
 import { logInToKeycloak } from "./utils";
 
@@ -32,4 +34,71 @@ test.describe("user management page", () => {
       );
     expect(nonColorViolations).toEqual([]);
   });
+
+  test("should create a new user", async ({ page, browserName }) => {
+    // Create programs
+    const program1 = await createRandomProgramArea(page);
+    const program2 = await createRandomProgramArea(page);
+
+    // Create user & assign to Program 1
+    await page.goto("/ecr-viewer/admin/user");
+
+    await expect(page.getByText("User management")).toBeVisible();
+
+    await page.getByText("Add new user").click();
+
+    await page.waitForURL("/ecr-viewer/admin/user/create");
+    await expect(page.getByRole("heading", { name: "Add user" })).toBeVisible();
+
+    const accessibilityScanResultsBase = await new AxeBuilder({
+      page,
+    }).analyze();
+    expect(accessibilityScanResultsBase.violations).toEqual([]);
+
+    page.getByLabel("EMAIL").fill(`${browserName}@test.test`);
+    const adminRadio = page.getByLabel("Standard", {exact: true});
+    await adminRadio.scrollIntoViewIfNeeded();
+    await adminRadio.dispatchEvent("click");
+
+    const checkboxProgram1 = await page.getByLabel(`Checkbox for program-${toKebabCase(program1)}`);  
+    await checkboxProgram1.scrollIntoViewIfNeeded();
+    await checkboxProgram1.dispatchEvent("click");
+
+    await page.getByRole("button", { name: "Add user" }).first().click();
+
+    // Check that user has been successfully/correctly created
+    await page.waitForURL("/ecr-viewer/admin/user");
+    await expect(page.getByText(`${browserName}@test.test`)).toBeVisible();
+    const row = page.locator("tr", {
+      has: page.getByText(`${browserName}@test.test`),
+    });
+    await expect(row.getByText("Standard")).toBeVisible();
+    await expect(page.getByText(program1)).toBeVisible();
+    await expect(page.getByText(program2)).not.toBeVisible();
+  }); 
 });
+
+const createRandomProgramArea = async (page: Page) => {
+  await page.goto("/ecr-viewer/admin/program");
+  await page.getByText("Create program area").click();
+  await page.waitForURL("/ecr-viewer/admin/program/create");
+
+  const checkboxesCond = await page.getByRole("checkbox").all();
+  const index = Math.floor(Math.random() * checkboxesCond.length);
+  const checkboxCond = checkboxesCond[index];
+  await checkboxCond.scrollIntoViewIfNeeded();
+  await checkboxCond.dispatchEvent("click");
+
+  await page.getByLabel("Program area name").fill(`Program ${index}`);
+  await page
+    .getByRole("button", { name: "Create program area" })
+    .first()
+    .click();
+
+  await page.waitForURL("/ecr-viewer/admin/program");
+  await expect(
+    page.getByText(`Program ${index}`, { exact: true })
+  ).toBeVisible();
+
+  return `Program ${index}`;
+};
