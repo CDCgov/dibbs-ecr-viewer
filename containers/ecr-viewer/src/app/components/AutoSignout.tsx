c8e0aa747e04b541c747e6cf7afc2eac953be9cd
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-06-04 10:38:02 -0400

    fix: always mount modal, make sure on top, avoid flickering

--- containers/ecr-viewer/src/app/components/AutoSignout.tsx
+++ containers/ecr-viewer/src/app/components/AutoSignout.tsx
@@ -12,7 +12,7 @@ import { signOut, useSession } from "next-auth/react";
 import Modal, { ModalRef } from "./modal/Modal";
 
 // Events we consider "activity" to keep the session alive
-const ACTIVE_EVENTS = ["click", "keypress", "scroll"];
+const ACTIVE_EVENTS = ["click", "keydown", "scroll"];
 
 // Seconds out from session expiration at which we'll warn the user
 const WARNING_DURATION = 90;
@@ -29,9 +29,6 @@ export const AutoSignout = () => {
   const [timeToExpireSecs, setTimeToExpireSecs] = useState(99999999999);
   const modalRef = useRef<ModalRef>(null);
 
-  // delay session update to make sure original action (e.g. signout) happens first
-  const updateInASec = () => setTimeout(update, 1000);
-
   // flip isActive to true on an event that we mark as activity
   useEffect(() => {
     if (isActive) return;
@@ -46,7 +43,8 @@ export const AutoSignout = () => {
   // refresh session if becomes active and then don't allow checking activity for 30 seconds
   useEffect(() => {
     if (isActive) {
-      updateInASec();
+      // delay session update to make sure original action (e.g. signout) happens first
+      setTimeout(update, 1000);
       const t = setTimeout(() => {
         setIsActive(false);
       }, 30 * 1000);
@@ -74,52 +72,52 @@ export const AutoSignout = () => {
   // Sign out and clear modal if needed
   useEffect(() => {
     if (timeToExpireSecs < 0) signOutGoHome();
-    if (modalRef.current?.modalIsOpen && timeToExpireSecs > WARNING_DURATION) {
-      modalRef.current.toggleModal();
+    if (timeToExpireSecs <= WARNING_DURATION && !isActive)
+      modalRef.current?.toggleModal(undefined, true);
+    if (timeToExpireSecs > WARNING_DURATION && modalRef.current?.modalIsOpen) {
+      modalRef.current.toggleModal(undefined, false);
     }
   }, [timeToExpireSecs]);
 
   return (
-    timeToExpireSecs < WARNING_DURATION && (
-      <Modal
-        isInitiallyOpen={true}
-        ref={modalRef}
-        id="session-expiring"
-        aria-labelledby="session-expiring-heading"
-        aria-describedby="session-expiring-description"
-        onClose={updateInASec}
-      >
-        <ModalHeading id="session-expiring-heading">
-          Session about to expire
-        </ModalHeading>
-        <p id="session-expiring-description">
-          Your session is about to expire due to inactivity.
-        </p>
-        <p>
-          {Math.max(0, timeToExpireSecs)} seconds remaining until you are signed
-          out.
-        </p>
-        <ModalFooter>
-          <ButtonGroup className="flex-justify-end">
-            <ModalToggleButton
-              modalRef={modalRef}
-              closer={true}
-              data-focus={true}
-            >
-              Extend session
-            </ModalToggleButton>
-            <ModalToggleButton
-              modalRef={modalRef}
-              closer={true}
-              outline={true}
-              className="padding-105"
-              onClick={signOutGoHome}
-            >
-              Sign out
-            </ModalToggleButton>
-          </ButtonGroup>
-        </ModalFooter>
-      </Modal>
-    )
+    <Modal
+      ref={modalRef}
+      zIndex={999999} // always on top, because this is rooted in the app, the position in markup is not semantic
+      id="session-expiring"
+      aria-labelledby="session-expiring-heading"
+      aria-describedby="session-expiring-description"
+    >
+      <ModalHeading id="session-expiring-heading">
+        Session about to expire
+      </ModalHeading>
+      <p id="session-expiring-description">
+        Your session is about to expire due to inactivity.
+      </p>
+      <p>
+        {Math.max(0, timeToExpireSecs)} seconds remaining until you are signed
+        out.
+      </p>
+      <ModalFooter>
+        <ButtonGroup className="flex-justify-end">
+          {/** clicking the button causes isActive to flip to true */}
+          <ModalToggleButton
+            modalRef={modalRef}
+            closer={true}
+            data-focus={true}
+          >
+            Extend session
+          </ModalToggleButton>
+          <ModalToggleButton
+            modalRef={modalRef}
+            closer={true}
+            outline={true}
+            className="padding-105"
+            onClick={signOutGoHome}
+          >
+            Sign out
+          </ModalToggleButton>
+        </ButtonGroup>
+      </ModalFooter>
+    </Modal>
   );
 };
