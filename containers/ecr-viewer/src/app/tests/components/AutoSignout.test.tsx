ee2a62cd919dad955b74011cd5d391570d6c84ff
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-06-05 11:47:33 -0400

    test: wip - unit tests

--- containers/ecr-viewer/src/app/tests/components/AutoSignout.test.tsx
+++ containers/ecr-viewer/src/app/tests/components/AutoSignout.test.tsx
@@ -1,7 +1,51 @@
+import { act, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useSession } from "next-auth/react";
+
+import { AutoSignout } from "@/app/components/AutoSignout";
+
+jest.mock("next-auth/react");
+
 describe("AutoSignout", () => {
-  it("updates session on activity");
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
 
-  it("shows warning when no activity and expiration approaching");
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+  it("updates session on activity", async () => {
+    const updateMock = jest.fn();
+    (useSession as jest.Mock).mockReturnValue({
+      update: updateMock,
+      data: {
+        expires: new Date(Date.now() + 3000).toISOString(),
+      },
+    });
 
-  it("force signs out user when session expires");
+    const { container } = render(<AutoSignout />);
+
+    expect(updateMock).not.toHaveBeenCalled();
+
+    const user = userEvent.setup();
+    await user.click(container);
+
+    act(() => {
+      jest.advanceTimersByTime(1100);
+    });
+
+    expect(updateMock).toHaveBeenCalledOnce();
+
+    // click again, shouldn't trigger update due to delay
+    await user.click(container);
+
+    act(() => {
+      jest.advanceTimersByTime(1100);
+    });
+    expect(updateMock).toHaveBeenCalledOnce();
+  });
+
+  it("shows warning when no activity and expiration approaching", () => {});
+
+  it("force signs out user when session expires", () => {});
 });
