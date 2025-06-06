c744fd499376520483ab8863a844d155bca3d909
Gordon Farrell <gordon@skylight.digital>
2025-06-06 10:34:24 -0600

    update header test

--- containers/ecr-viewer/src/app/tests/components/Header.test.tsx
+++ containers/ecr-viewer/src/app/tests/components/Header.test.tsx
@@ -6,15 +6,18 @@ import "@testing-library/jest-dom";
 import Header from "@/app/components/Header";
 
 describe("Header component", () => {
-  it("renders the logo image with correct alt text", () => {
+  it("renders the header and logo image with correct alt text", () => {
     render(
       <Header>
         <p>I am a child element</p>
       </Header>,
     );
-    const logo = screen.getByAltText("eCR Viewer Logo");
+    const logo = screen.getByAltText("DIBBs Logo");
     expect(logo).toBeInTheDocument();
-    expect(logo.getAttribute("src")).toContain("ecr-viewer-logo.png");
+    expect(logo.getAttribute("src")).toContain("dibbs-logo.png");
+
+    // Check that it has the correct text
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('eCR Viewer');
   });
 
   it("renders children passed to the component", () => {
