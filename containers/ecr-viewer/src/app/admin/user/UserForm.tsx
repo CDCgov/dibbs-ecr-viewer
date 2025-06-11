5298d4150c96aad781a6d558caa3fcda951c4196
angelathe <angela.the@skylight.digital>
2025-06-11 11:13:22 -0400

    fix: db shouldnt save program assignments to admin users

--- containers/ecr-viewer/src/app/admin/user/UserForm.tsx
+++ containers/ecr-viewer/src/app/admin/user/UserForm.tsx
@@ -91,7 +91,7 @@ export const UserForm = ({
         const res = await submitAction(
           email.trim(),
           userType,
-          selectedPrograms,
+          userType === "admin" ? [] : selectedPrograms // admins should not be saved with assigned programs
         );
         if (!res.error)
           createToast(`${email.trim()} successfully saved`, "success");
