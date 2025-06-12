ab6248d1001a660a6ed68a1b3606324d923a21af
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-06-12 14:54:38 -0400

    fix: change lab and address pks to be compound

---
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/extended/20250612143900_compound_pks.ts
@@ -0,0 +1,42 @@
+import { Kysely } from "kysely";
+
+import { AnyDb } from "@/app/data/metadataDb/database";
+import { dbNamespace, dbSchema } from "@/app/data/metadataDb/utils/db-config";
+
+/**
+ * Change the pks to be compound instead of just uuid
+ * @param db - the database connection
+ */
+export async function up(db: Kysely<AnyDb>): Promise<void> {
+  if (dbSchema() !== "extended") {
+    console.log(`${dbSchema()} schema detected. Skipping extended migration.`);
+    return;
+  }
+
+  const _db = db.withSchema(dbNamespace());
+
+  await _db.schema
+    .alterTable("ecr_labs")
+    .addPrimaryKeyConstraint("ecr_labs_pk_uuid_eicr_id", ["uuid", "eicr_id"])
+    .execute();
+  await _db.schema
+    .alterTable("patient_address")
+    .addPrimaryKeyConstraint("ecr_labs_pk_uuid_eicr_id", ["uuid", "eicr_id"])
+    .execute();
+}
+
+/**
+ * Remove the compound pks
+ * @param db - the database connection
+ */
+export async function down(db: Kysely<AnyDb>): Promise<void> {
+  const _db = db.withSchema(dbNamespace());
+  await _db.schema
+    .alterTable("ecr_labs")
+    .dropConstraint("ecr_labs_pk_uuid_eicr_id")
+    .execute();
+  await _db.schema
+    .alterTable("patient_address")
+    .dropConstraint("ecr_labs_pk_uuid_eicr_id")
+    .execute();
+}
