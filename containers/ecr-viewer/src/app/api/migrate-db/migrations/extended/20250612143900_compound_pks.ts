1b3e7577238592508b998a190377064bbb7e023a
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-06-12 16:10:43 -0400

    fix: wip - fighting sql server

--- containers/ecr-viewer/src/app/api/migrate-db/migrations/extended/20250612143900_compound_pks.ts
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/extended/20250612143900_compound_pks.ts
@@ -1,7 +1,11 @@
-import { Kysely } from "kysely";
+import { Kysely, sql } from "kysely";
 
 import { AnyDb } from "@/app/data/metadataDb/database";
-import { dbNamespace, dbSchema } from "@/app/data/metadataDb/utils/db-config";
+import {
+  dbDialect,
+  dbNamespace,
+  dbSchema,
+} from "@/app/data/metadataDb/utils/db-config";
 
 /**
  * Change the pks to be compound instead of just uuid
@@ -15,26 +19,31 @@ export async function up(db: Kysely<AnyDb>): Promise<void> {
 
   const _db = db.withSchema(dbNamespace());
 
-  await _db.schema
-    .alterTable("ecr_labs")
-    .dropConstraint("ecr_labs_pkey")
-    .execute();
-  await _db.schema
-    .alterTable("ecr_labs")
-    .addPrimaryKeyConstraint("ecr_labs_pk_uuid_eicr_id", ["uuid", "eicr_id"])
-    .execute();
+  for (const table of ["ecr_labs", "patient_address"]) {
+    let pkey_name = `${table}_pkey`;
 
-  await _db.schema
-    .alterTable("patient_address")
-    .dropConstraint("patient_address_pkey")
-    .execute();
-  await _db.schema
-    .alterTable("patient_address")
-    .addPrimaryKeyConstraint("patient_address_pk_uuid_eicr_id", [
-      "uuid",
-      "eicr_id",
-    ])
-    .execute();
+    if (dbDialect() === "sqlserver") {
+      // sql server key names are randomized, find it to delete it
+      // https://learn.microsoft.com/en-us/sql/relational-databases/tables/delete-primary-keys?view=sql-server-ver17
+      const { name } = await db
+        .selectFrom("sys.key_constraints")
+        .select("name")
+        .where("type", "=", "PK")
+        .where(sql`OBJECT_NAME(parent_object_id)`, "=", table)
+        .executeTakeFirstOrThrow();
+      pkey_name = name;
+    }
+
+    await _db.schema.alterTable(table).dropConstraint(pkey_name).execute();
+    await _db.schema
+      .alterTable(table)
+      .alterColumn("eicr_id", (cb) => cb.setNotNull())
+      .execute();
+    await _db.schema
+      .alterTable(table)
+      .addPrimaryKeyConstraint(`${table}_pk_uuid_eicr_id`, ["uuid", "eicr_id"])
+      .execute();
+  }
 }
 
 /**
@@ -49,6 +58,10 @@ export async function down(db: Kysely<AnyDb>): Promise<void> {
     .dropConstraint("ecr_labs_pk_uuid_eicr_id")
     .execute();
   await _db.schema
+    .alterTable("ecr_lbas")
+    .alterColumn("eicr_id", (cb) => cb.dropNotNull())
+    .execute();
+  await _db.schema
     .alterTable("ecr_labs")
     .addPrimaryKeyConstraint("ecr_labs_pkey", ["uuid"])
     .execute();
@@ -56,6 +69,10 @@ export async function down(db: Kysely<AnyDb>): Promise<void> {
   await _db.schema
     .alterTable("patient_address")
     .dropConstraint("patient_address_pk_uuid_eicr_id")
+    .execute();
+  await _db.schema
+    .alterTable("patient_address")
+    .alterColumn("eicr_id", (cb) => cb.dropNotNull())
     .execute();
   await _db.schema
     .alterTable("patient_address")
