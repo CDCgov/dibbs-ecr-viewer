bb5eb45d403c723e9abe5628cc234a04cd45dfae
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-06-13 14:12:46 -0400

    fix: harden dev log out

--- containers/ecr-viewer/next.config.js
+++ containers/ecr-viewer/next.config.js
@@ -27,6 +27,9 @@ const nextConfig = {
   env: {
     BASE_PATH: basePath,
   },
+
+  // next auth useSession doesn't double mount nicely
+  reactStrictMode: false,
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
