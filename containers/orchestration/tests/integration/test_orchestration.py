7a41f1fb4cfa9d13934ee249bfae206ef9279c6d
Gordon Farrell <gordon@skylight.digital>
2025-06-13 15:39:41 -0600

    remove ancient WS test cruft, will make a ticket to remove the actual endpoint soon

--- containers/orchestration/tests/integration/test_orchestration.py
+++ containers/orchestration/tests/integration/test_orchestration.py
@@ -288,26 +288,3 @@ def test_process_message_hl7(setup):
         f"Expected status code 200, but got ${orchestration_response.status_code}. Response content is ${orchestration_response.content}"
     )
     assert orchestration_response.json()["message"] == "Processing succeeded!"
-
-
-@pytest.mark.asyncio
-@pytest.mark.integration
-async def test_websocket_process_message_endpoint(setup):
-    expected_response_message = {}
-    client = TestClient(app)
-    # Pull in and read test zip file
-    with open(
-        Path(__file__).parent.parent / "assets" / "test_zip.zip",
-        "rb",
-    ) as file:
-        test_zip = file.read()
-
-    # Create fake websocket connection
-    with client.websocket_connect("/process-ws") as websocket:
-        # Send zip into fake connection, triggering process-ws endpoint
-        websocket.send_bytes(test_zip)
-
-        # Pull response message from websocket connection like frontend would
-        messages = websocket.receive_json()
-
-    assert messages == expected_response_message
