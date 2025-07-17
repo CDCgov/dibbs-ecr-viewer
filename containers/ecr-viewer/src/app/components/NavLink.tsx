0f343c4bd7e0ad2006e03fe3bb7ecc075f85be58
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-07-17 09:01:19 -0400

    fix: wip - nav link states

---
+++ containers/ecr-viewer/src/app/components/NavLink.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { ReactNode } from "react";
+
+import classnames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+/**
+ * Nav link component for the eCR Viewer Header.
+ * This component renders a link with proper styling
+ * @param props react props
+ * @param props.href the link to visit
+ * @param props.children link content
+ * @returns The header nav link
+ */
+const NavLink = ({ href, children }: { href: string; children: ReactNode }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={classnames("usa-nav__link", {
+        "active-page": pathname.includes(href),
+      })}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
