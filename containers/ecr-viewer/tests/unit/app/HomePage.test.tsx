a9ebe132e216093f3613e5dd31eef0d5d60e6054
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-08-06 15:10:55 -0400

    Update containers/ecr-viewer/tests/unit/app/HomePage.test.tsx
    Co-authored-by: Gordon Farrell <93161643+gordonfarrell@users.noreply.github.com>

--- containers/ecr-viewer/tests/unit/app/HomePage.test.tsx
+++ containers/ecr-viewer/tests/unit/app/HomePage.test.tsx
@@ -14,7 +14,7 @@ import { PageSearchParams } from "@/app/utils/search-param-utils";
 const resolveParams = (
   v: PageSearchParams,
 ): { searchParams: Promise<PageSearchParams> } => ({
-  searchParams: new Promise((resolve) => resolve(v)),
+  searchParams: Promise.resolve(v)
 });
 
 jest.mock("@/app/services/listEcrDataService", () => {
