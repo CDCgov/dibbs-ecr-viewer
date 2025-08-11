2d689919f2f8cf29094493971234d7a262724b7d
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-08-11 14:41:17 -0400

    fix: cleanup, DataDisplayList

--- containers/ecr-viewer/src/app/view-data/services/labsService.tsx
+++ containers/ecr-viewer/src/app/view-data/services/labsService.tsx
@@ -44,7 +44,7 @@ import {
   toKebabCase,
 } from "@/app/utils/format-utils";
 import {
-  DataDisplay,
+  DataDisplayList,
   DisplayDataProps,
 } from "@/app/view-data/components/DataDisplay";
 import EvaluateTable, {
@@ -678,37 +678,28 @@ const getLabsContent = (
   ];
 
   const { availableData, unavailableData } = evaluateData(rrInfo);
+  const h6ClassName = "bg-gray-5 margin-x-neg-205 padding-y-2 padding-x-205";
 
   return (
     <>
       {labTableDiagnostic}
       {labTableOrganisms}
-      {availableData.length && (
+
+      {availableData.length > 0 && (
         <h6
           // inline styling to overwrite usa-prose nested style
           style={{ marginTop: "-1rem" }}
-          className="bg-gray-5 margin-x-neg-205 padding-y-2 padding-x-205"
+          className={h6ClassName}
         >
           Available lab information
         </h6>
       )}
-      {availableData.map((item, i) => (
-        <DataDisplay
-          key={`${item.title}-${item.value}`}
-          item={{ ...item, dividerLine: i + 1 !== availableData.length }}
-        />
-      ))}
-      {unavailableData.length && (
-        <h6 className="bg-gray-5 margin-x-neg-205 padding-y-2 padding-x-205">
-          Unavailable lab information
-        </h6>
+      <DataDisplayList items={availableData} />
+
+      {unavailableData.length > 0 && (
+        <h6 className={h6ClassName}>Unavailable lab information</h6>
       )}
-      {unavailableData.map((item, i) => (
-        <DataDisplay
-          key={`${item.title}-${item.value}`}
-          item={{ ...item, dividerLine: i + 1 !== unavailableData.length }}
-        />
-      ))}
+      <DataDisplayList items={unavailableData} />
     </>
   );
 };
