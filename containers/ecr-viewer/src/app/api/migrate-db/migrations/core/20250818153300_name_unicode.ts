a240230b7b2ed70c59fa2602362102cdc2488fc7
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-08-18 19:20:26 -0400

    fix: don't drop the null constraint

--- containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250818153300_name_unicode.ts
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/core/20250818153300_name_unicode.ts
@@ -1,4 +1,4 @@
-import { Kysely, sql } from "kysely";
+import { CompiledQuery, Kysely } from "kysely";
 
 import { AnyDb } from "@/app/data/metadataDb/database";
 import { dbDialect, dbNamespace } from "@/app/data/metadataDb/utils/db-config";
@@ -12,18 +12,20 @@ export async function up(db: Kysely<AnyDb>): Promise<void> {
   if (dbDialect() !== "sqlserver") return;
 
   const schema = dbNamespace();
-  const _db = db.withSchema(schema);
 
-  // sql server only allows you to alter one column at a time
-  await _db.schema
-    .alterTable("ecr_data")
-    .alterColumn("first_name", (cb) => cb.setDataType(sql`nvarchar(255)`))
-    .execute();
-
-  await _db.schema
-    .alterTable("ecr_data")
-    .alterColumn("last_name", (cb) => cb.setDataType(sql`nvarchar(255)`))
-    .execute();
+  // sql server only allows you to alter one column at a time and requires the full definition
+  // kysely only allows you to alter one thing about a column at a time
+  // these do not play well together...
+  await db.executeQuery(
+    CompiledQuery.raw(
+      `ALTER TABLE [${schema}].[ecr_data] ALTER COLUMN first_name nvarchar(255) NOT NULL`,
+    ),
+  );
+  await db.executeQuery(
+    CompiledQuery.raw(
+      `ALTER TABLE [${schema}].[ecr_data] ALTER COLUMN last_name nvarchar(255) NOT NULL`,
+    ),
+  );
 }
 
 /**
@@ -34,13 +36,15 @@ export async function down(db: Kysely<AnyDb>): Promise<void> {
   // This migration only applies to sql server
   if (dbDialect() !== "sqlserver") return;
 
-  const _db = db.withSchema(dbNamespace());
-  await _db.schema
-    .alterTable("ecr_data")
-    .alterColumn("last_name", (cb) => cb.setDataType("varchar(255)"))
-    .execute();
-  await _db.schema
-    .alterTable("ecr_data")
-    .alterColumn("first_name", (cb) => cb.setDataType("varchar(255)"))
-    .execute();
+  const schema = dbNamespace();
+  await db.executeQuery(
+    CompiledQuery.raw(
+      `ALTER TABLE [${schema}].[ecr_data] ALTER COLUMN first_name varchar(255) NOT NULL`,
+    ),
+  );
+  await db.executeQuery(
+    CompiledQuery.raw(
+      `ALTER TABLE [${schema}].[ecr_data] ALTER COLUMN last_name varchar(255) NOT NULL`,
+    ),
+  );
 }
