b82f3bbb5262d949e456063abe6d106e07341db6
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-08-20 08:38:00 -0400

    test: slightly less weird complex name

--- containers/ecr-viewer/tests/e2e/dual/ecr-library.spec.ts
+++ containers/ecr-viewer/tests/e2e/dual/ecr-library.spec.ts
@@ -125,7 +125,7 @@ test.describe("ecr library page", () => {
       await expect(page.getByLabel("Page 2")).not.toBeVisible();
       await expect(page.getByText("Showing 1-3")).toBeVisible();
       // Regex to make sure name is formatted correctly
-      await expect(page.getByText(/McRendar🐨/)).toBeVisible();
+      await expect(page.getByText(/O'Rendar木/)).toBeVisible();
       await expect(page.locator("tbody > tr")).toHaveCount(3);
     });
 
