a0c633b308f47ba28d3e574f8cdd824183e46a08
Angela The <angela.the@skylight.digital>
2025-10-03 11:57:49 -0700

    fix: Remove required marker from Create User - select program areas (#1170)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- containers/ecr-viewer/src/app/admin/user/UserForm.tsx
+++ containers/ecr-viewer/src/app/admin/user/UserForm.tsx
@@ -269,10 +269,7 @@ export const ProgramFieldSet = ({
     <FieldSet legend="Program area access">
       <span>
         {isStandardUser ? (
-          <>
-            Select one or more program areas
-            <RequiredMarker />
-          </>
+          <>Select one or more program areas</>
         ) : (
           <>Admins will be able to see all program areas and conditions</>
         )}
