1c2ef200064cd02ca6710afffdb7eaea41259df2
Angela The <angela.the@skylight.digital>
2025-11-19 06:40:32 -0800

    fix: Update col length for `test_result_qualitative` (#1223)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- containers/ecr-viewer/src/app/api/migrate-db/migrations/extended/index.ts
+++ containers/ecr-viewer/src/app/api/migrate-db/migrations/extended/index.ts
@@ -2,4 +2,5 @@ export default {
   "19700101000000_initial.ts": require("./19700101000000_initial"),
   "20250505100700_ecr_fks.ts": require("./20250505100700_ecr_fks"),
   "20250612143900_compound_pks.ts": require("./20250612143900_compound_pks"),
+  "20251114100000_lab_qual_varchar_max": require("./20251114100000_lab_qual_varchar_max"),
 };
