77c09a3088af19e8bea06df4779dda64af258bc5
Angela The <angela.the@skylight.digital>
2025-12-03 07:39:09 -0800

    feat: Add version number (#1240)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- containers/ecr-viewer/tests/unit/app/components/UserMenu.test.tsx
+++ containers/ecr-viewer/tests/unit/app/components/UserMenu.test.tsx
@@ -23,19 +23,20 @@ describe("UserMenu component", () => {
   }
 
   it("renders the profile image with correct alt text", () => {
-    renderWithSession(<UserMenu user={mockUser} />);
+    renderWithSession(<UserMenu user={mockUser} version="vTest" />);
     const profileImage = screen.getByRole("img");
     expect(profileImage).toBeInTheDocument();
     expect(profileImage.getAttribute("aria-label")).toContain("User Menu");
   });
 
   it("toggles the menu when button is clicked and displays user info", () => {
-    renderWithSession(<UserMenu user={mockUser} />);
+    renderWithSession(<UserMenu user={mockUser} version="vTest" />);
     const button = screen.getByRole("button");
     fireEvent.click(button);
     expect(
       screen.getByText("kyle.katarn@fakestarwarsemail.bananas.com"),
     ).toBeInTheDocument();
     expect(screen.getByText("Admin")).toBeInTheDocument();
+    expect(screen.getByText(/vTest/i)).toBeInTheDocument();
   });
 });
