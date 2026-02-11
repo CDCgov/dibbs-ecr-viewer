f8fc2ddebd5c1217df6b5f9805abe6287bf85dc0
laura-puerto-skylight <laura.puerto@skylight.digital>
2026-02-11 13:44:53 -0800

    Adding rest of unit tests

--- containers/ecr-viewer/tests/unit/app/utils/ecrid-utils.test.tsx
+++ containers/ecr-viewer/tests/unit/app/utils/ecrid-utils.test.tsx
@@ -1,12 +1,44 @@
-import { resolveEcrId } from "@/app/utils/ecrid-utils";
+import { getEcrIdFromIdentifier, resolveEcrId } from "@/app/utils/ecrid-utils";
 
-describe("ECR id utils", () => {
+describe("ECR ID Utils", () => {
   describe("getEcrIdFromIdentifier", () => {
-    //it("should return root if only root has a valu");
+    it("should return root (from value with no prefix) if system is urn:ietf:rfc:3986", () => {
+      const identifier = {
+        system: "urn:ietf:rfc:3986",
+        value: "urn:oid:2.16.840.1.113883.9.9.9.9.9",
+      };
+
+      const result = getEcrIdFromIdentifier(identifier);
+      const expectedEcrId = "2.16.840.1.113883.9.9.9.9.9";
+      expect(result).toBe(expectedEcrId);
+    });
+
+    it("should return extension (from value) if system is null flavor", () => {
+      const identifier = {
+        system: "http://terminology.hl7.org/CodeSystem/v3-NullFlavor",
+        value: "db734647-fc99-424c-a864-7e3cda82e703",
+      };
+
+      const result = getEcrIdFromIdentifier(identifier);
+      const expectedEcrId = "db734647-fc99-424c-a864-7e3cda82e703";
+      expect(result).toBe(expectedEcrId);
+    });
+
+    it("should return the concatenated root (no prefix) and extension if both system and value are valid", () => {
+      const identifier = {
+        system: "urn:oid:2.16.840.1.113883.9.9.9.9.9",
+        value: "db734647-fc99-424c-a864-7e3cda82e703",
+      };
+
+      const result = getEcrIdFromIdentifier(identifier);
+      const expectedEcrId =
+        "2.16.840.1.113883.9.9.9.9.9^db734647-fc99-424c-a864-7e3cda82e703";
+      expect(result).toBe(expectedEcrId);
+    });
   });
 
   describe("resolveEcrId", () => {
-    it("should return the full id with both root and extension if present", () => {
+    it("should return the full id with both root (no prefix) and extension if present", () => {
       const root = "urn:oid:2.16.840.1.113883.9.9.9.9.9";
       const extension = "db734647-fc99-424c-a864-7e3cda82e703";
 
@@ -16,7 +48,7 @@ describe("ECR id utils", () => {
       expect(result).toBe(expectedEcrId);
     });
 
-    it("should return only the root when extension is missing", () => {
+    it("should return only the root (no prefix) when extension is missing", () => {
       const root = "urn:oid:2.16.840.1.113883.9.9.9.9.9";
       const extension = "";
 
