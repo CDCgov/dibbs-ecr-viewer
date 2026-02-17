0dc3ee4f761455c330af5bba24758602f9b5cede
Gordon Farrell <gordon@skylight.digital>
2026-02-17 10:46:44 -0700

    tweak how we do sass imports to work with new next, few other updates

--- containers/ecr-viewer/next.config.js
+++ containers/ecr-viewer/next.config.js
@@ -8,9 +8,10 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const nextConfig = {
   sassOptions: {
-    includePaths: [
+    loadPaths: [
       path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
     ],
+    api: 'legacy',
     quietDeps: true,
   },
   experimental: {
@@ -27,7 +28,6 @@ const nextConfig = {
   env: {
     BASE_PATH: basePath,
   },
-  turbopack: {},
 
   // next auth useSession doesn't double mount nicely
   reactStrictMode: false,
