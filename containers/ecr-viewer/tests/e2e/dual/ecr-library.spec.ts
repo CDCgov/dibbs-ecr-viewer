89b5bc8306c00735920a33df25c210657d593b4f
laura-puerto-skylight <laura.puerto@skylight.digital>
2026-04-10 09:52:31 -0700

    Another fix for e2e tests

--- containers/ecr-viewer/tests/e2e/dual/ecr-library.spec.ts
+++ containers/ecr-viewer/tests/e2e/dual/ecr-library.spec.ts
@@ -224,18 +224,23 @@ test.describe("ecr library page", () => {
 
   test.describe("eCR grouping", () => {
     test("expanding group", async ({ page }) => {
-      const expandButton = page
-        .getByRole("button", { name: "View Related eCRs" })
-        .first();
+      // Find the button for Mon Mothma
+      const patientCell = page
+        .locator(".patient-name-cell")
+        .filter({ hasText: "Mon Mothma" });
+
+      // Find the button within that patient cell
+      const expandButton = patientCell.getByRole("button", {
+        name: "View Related eCRs",
+      });
 
       await expect(expandButton).toBeVisible();
       await expandButton.click();
       await expect(page.getByRole("row", { level: 2 })).toHaveCount(2);
 
       // collapse it back down
-      await page
+      await patientCell
         .getByRole("button", { name: "Hide Related eCRs" })
-        .first()
         .click();
       await expect(page.getByRole("row", { level: 2 })).toHaveCount(0);
     });
