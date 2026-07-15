68d91d162a62a6c848c00a6468be0aac976d5f88
laura-puerto-skylight <laura.puerto@skylight.digital>
2026-07-15 08:49:47 -0700

    Minor change back

--- containers/ecr-viewer/tests/unit/app/api/process-ecr/service.test.ts
+++ containers/ecr-viewer/tests/unit/app/api/process-ecr/service.test.ts
@@ -36,7 +36,7 @@ describe("orchestrationRequest", () => {
     id: "123",
     identifier: { system: "hello", value: "world" },
   };
-  const mockMetadata = { facility_name: "Hospital A" };
+  const mockMetadata = { key: "value" };
 
   let mockPool: Interceptable;
 
