94cfa5f3940ac6d75db31c2d090db549dc231fd6
angelathe <angela.the@skylight.digital>
2026-08-07 11:33:49 -0400

    fix: user type validation on edit user page

--- containers/ecr-viewer/src/app/admin/user/edit/page.tsx
+++ containers/ecr-viewer/src/app/admin/user/edit/page.tsx
@@ -48,7 +48,7 @@ const EditUserPage = async ({
   );
 
   const isValidUserType = (value: string): value is UserType => {
-    return value === USER_TYPE.ADMIN || value === USER_TYPE.STANDARD;
+    return value === USER_TYPE.ADMIN || value === USER_TYPE.PROG_ADMIN || value === USER_TYPE.STANDARD;
   };
 
   const users = await listUsers();
