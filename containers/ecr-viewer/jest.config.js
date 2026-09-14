e1a3bc3f602a3cbcd739bb561e2f38c705f9355f
Austin Hall <austin.hall@skylight.digital>
2026-09-14 19:52:13 -0400

    transform uuid module

--- containers/ecr-viewer/jest.config.js
+++ containers/ecr-viewer/jest.config.js
@@ -35,6 +35,6 @@ const customJestConfig = {
 module.exports = async () => ({
   ...(await createJestConfig(customJestConfig)()),
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!(jose|@azure|@typespec|sanitize-html|htmlparser2)/)",
+    "<rootDir>/node_modules/(?!(jose|@azure|@typespec|sanitize-html|htmlparser2|uuid)/)",
   ],
 });
